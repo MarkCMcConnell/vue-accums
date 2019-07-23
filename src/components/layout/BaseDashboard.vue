76a9acf6950919fbbb3531a168147126743b5aa8
Mark McConnell <mcmcconn@gmail.com>
2019-07-23 14:10:33 -0400

    Building reusable dashboard

--- src/components/layout/BaseDashboard.vue
+++ src/components/layout/BaseDashboard.vue
@@ -29,8 +29,12 @@ export default {
     memberData: {
       type: Array,
       required: true
-		},
-		dashboardFields: {
+    },
+    dataType: {
+      type: String,
+      required: true
+    },
+		dataFields: {
 			type: Array,
 			required: true
 		},
@@ -48,12 +52,24 @@ export default {
     }
   },
   computed: {
-    formatCoverages () {
-      let coverages
+    formatData () {
+      let data
       let dataValues = []
 
+      if (this.dataType === 'gaps') {
+        data = this.memberData.filter(gap => {
+          return gap.SpecificType === 'GC'
+        })  
+      }
+
+      if (this.dataType === 'accums') {
+        data = this.memberData.filter(accum => {
+          return accum.SpecificType !== 'GC'
+        })
+      }
+
       if (this.yearRange) {
-        coverages = this.memberData.filter(coverage => {
+        data = this.memberData.filter(coverage => {
           const currentYear = new Date().getFullYear()
           let date = this.convertJSONDateToDateString(coverage.EffectiveDate)
           
@@ -64,11 +80,59 @@ export default {
       for (let obj of coverages) {
         let data = {}
 
-        for (let value of this.coverageFields) {
+        for (let value of this.dataFields) {
           let currentKey = obj[value.field]
+          // General conversion
           // Capture dates and convert /Date()/ format to human readable
           if (currentKey.substring(0, 5) === '/Date') {
             currentKey = this.convertJSONDateToDateString(currentKey)
+          }
+          // ACCUMS conversions
+          if (value.field === 'IndividualAccum') {
+            if (currentKey) {
+              currentKey = 'Individual'
+            } else {
+              currentKey = 'Family'
+            }
+          }
+
+          if (value.field === 'CoverageType') {
+            // Use this switch statement to customize or add values
+            switch (currentKey) {
+              case 'M':
+                currentKey = 'Medical'
+                break
+              case 'D':
+                currentKey = 'Dental'
+                break
+              default:
+                currentKey = 'Unknown'
+                break
+            }
+          }
+
+          if (field.field === 'CurrentAmount') {
+            currentKey = this.formatCurrency(obj.CurrentAmount)
+          }
+
+          if (field.field === 'MaximumAmount') {
+            currentKey = this.formatCurrency(obj.MaximumAmount)
+          }
+
+          if (field.field === 'ProgressBar') {
+            currentKey = this.calculatePercent(
+              obj.CurrentAmount,
+              obj.MaximumAmount
+            )
+          }
+          // GAPS specific - Convert CovBenefit abbreviation to full word
+          if (value.field === 'CovBenefit') {
+            if (currentKey === 'Y') {
+              currentKey = 'Yes'
+            }
+            if (currentKey === 'N') {
+              currentKey === 'No'
+            }
           }
           // Avoid setting an empty row
           if (currentKey !== '' && currentKey !== undefined) {
